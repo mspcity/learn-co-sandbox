20fa6242f328f7c32edbaa47ed0b69a939387fa6
Youngmin Park <codepark2019@gmail.com>
2021-06-12 00:21:07 +0000

    Automatically backed up by Learn

---
+++ hamburger.rb
@@ -0,0 +1,8 @@
+toppings = ["pickles", "mushrooms", "bacon"]
+ 
+def hamburger(toppings)
+  toppings.map do |topping|
+    puts "I love #{topping} on my burgers!"
+  end
+end
+hamburger(toppings)
---
+++ learning-repetition.rb
@@ -0,0 +1,64 @@
+# magic_exit_number = 7
+# count = 0
+# while count < 10 do
+#   break if count == magic_exit_number
+#   puts "I am the #{count}, I love to count!"
+#   count = count + 1
+# end
+
+# magic_exit_number = 7
+# count = 0
+# while count < 10 && count != magic_exit_number
+#   puts "I am the #{count}, I love to count!"
+#   count = count + 1
+# end
+
+# count = 0
+# n = 2
+# while count <= n do
+#   puts "I ran."
+#   count = count + 1
+# end
+
+# 3.times do
+#   puts "i ran."
+# end
+
+# count = 0
+# n = 3
+# loop do
+#   break if count >=n
+#   puts "I ran."
+#   count += 1
+# end
+
+# count = 0
+# n = 3
+# while true do
+#   break if count >= n
+#   puts "I ran."
+#   count += 1
+# end
+
+# counter = 0
+# until counter == 20
+# puts "The current number is less than 20."
+# counter += 1
+# end
+
+# def say_hello_world_five_times
+#   puts "Hello World!"
+#   puts "Hello World!"
+#   puts "Hello World!"
+#   puts "Hello World!"
+#   puts "Hello World!"
+# end
+
+# say_hello_world_five_times
+
+def say_hello_world
+  puts "Hello World!"
+end
+
+
+
